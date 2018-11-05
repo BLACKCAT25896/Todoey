3b4a2206e62f6c2327c6b5a889eb17c769e8d3c4
BLACKCAT25896 <joykamrujjaman59@gmail.com>
2018-11-06 00:38:26 +0600

    add mvc

--- Todoey/Controler/ToDoListViewController.swift
+++ Todoey/Controler/ToDoListViewController.swift
@@ -9,10 +9,23 @@
 import UIKit
 
 class ToDoListViewController: UITableViewController {
-    var itemArray = ["jaman", "muri","khay"]
-
+    var itemArray = [Item]()
+    let defaults = UserDefaults.standard
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        let newItem  = Item()
+        newItem.title = "Joy"
+        itemArray.append(newItem)
+        
+        let newItem2  = Item()
+        newItem2.title = "Muri Khay"
+        itemArray.append(newItem2)
+        
+        if let items = UserDefaults.standard.array(forKey: "ToDoListArray") as? [Item] {
+            itemArray = items
+        }
         
     }
     //MARK - Table View Datasource Method
@@ -21,25 +34,26 @@ class ToDoListViewController: UITableViewController {
     }
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell",for: indexPath)
-        cell.textLabel?.text = itemArray[indexPath.row]
+        
+        let item = itemArray[indexPath.row]
+        cell.accessoryType = item.done ? .checkmark : .none
+        
         return cell
         
     }
     //MARK - TableView Delegate Method
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark{
-            tableView.cellForRow(at: indexPath)?.accessoryType = .none
-            
-        }else{
-            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
-        }
+        
+        itemArray[indexPath.row].done = !itemArray[indexPath.row].done
+        
+        tableView.reloadData()
         tableView.deselectRow(at: indexPath, animated: true)
     }
     
     //MARK - Add New Item
     
-
-
+    
+    
     @IBAction func AddButtonPressed(_ sender: UIBarButtonItem) {
         
         
@@ -47,7 +61,12 @@ class ToDoListViewController: UITableViewController {
         let alert = UIAlertController(title: "Add New Todeoy Item", message: "", preferredStyle: .alert)
         let action = UIAlertAction(title: "Add Item", style: .default) { (action) in
             // what will happen here once user will click our UITodeoy system
-            self.itemArray.append(textField.text!)
+            
+            let newItem = Item()
+            newItem.title = textField.text!
+            self.itemArray.append(newItem)
+            
+            self.defaults.set(self.itemArray, forKey: "ToDoListArray")
             self.tableView.reloadData()
         }
         alert.addTextField { (alertTextField) in
